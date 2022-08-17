c9d29a9e91a82431f15ca1352932a45ed36e33ae
SouravsGithub <souravsemail2001@gmail.com>
2022-08-17 15:09:33 +0530

    made the numbers display as you click on them

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-const result = Number(
-  document.querySelector(".calculator-display").textContent
-);
+let result = document.querySelector(".calculator-display");
 const keys = document.querySelector(".calculator-keys");
 
 keys.addEventListener("click", function (event) {
-  const key = event.target;
-  const action = key.dataset.action;
-  if (!action) {
-    console.log("number key!");
+  if (event.target.matches("button")) {
+    const key = event.target;
+    const action = key.dataset.action;
+    const KeyContent = key.textContent;
+    const displayedNum = result.textContent;
+    if (!action) {
+      if (displayedNum === "0") {
+        result.textContent = KeyContent;
+      } else {
+        result.textContent = result.textContent + KeyContent;
+      }
+    }
+    if (action === "decimal") {
+      result.textContent = result.textContent + KeyContent;
+    }
   }
-  if (
-    action === "add" ||
-    action === "subtract" ||
-    action === "multiply" ||
-    action === "divide"
-  ) {
-    console.log("operator key");
-  }
-  if (action === "clear") {
-    console.log("clear key!");
-  }
-  if (action === "calculate") {
-    console.log("equal key!");
-  }
-  if (action === "decimal") {
-    console.log("decimal key!");
-  }
+
+  // if (
+  //   action === "add" ||
+  //   action === "subtract" ||
+  //   action === "multiply" ||
+  //   action === "divide"
+  // ) {
+  //   console.log("operator key");
+  // }
+  // if (action === "clear") {
+  //   console.log("clear key!");
+  // }
+  // if (action === "calculate") {
+  //   console.log("equal key!");
+  // }
+  // if (action === "decimal") {
+  //   console.log("decimal key!");
+  // }
 });
